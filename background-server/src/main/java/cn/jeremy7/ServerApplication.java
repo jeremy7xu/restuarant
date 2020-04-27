0ae2127f6218c0cd4f5c40f99236224f9e32471f
jeremy <xuhao@DIDI.com>
2020-04-27 09:21:46 +0800

    基本配置文件

---
+++ background-server/src/main/java/cn/jeremy7/ServerApplication.java
@@ -0,0 +1,15 @@
+package cn.jeremy7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author didi
+ */
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
